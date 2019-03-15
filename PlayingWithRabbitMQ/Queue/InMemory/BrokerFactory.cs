7cf60bb55a4bba323f191d25071bbf7b103ed19e
Balázs Keresztúri <19balazs86@gmail.com>
2019-03-15 19:01:16 +0000

    Make the in-memory BrokerFactory independent from QueueMessageAttribute.

--- PlayingWithRabbitMQ/Queue/InMemory/BrokerFactory.cs
+++ PlayingWithRabbitMQ/Queue/InMemory/BrokerFactory.cs
@@ -3,50 +3,22 @@ using System.Collections.Concurrent;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Reflection;
 using System.Threading.Tasks.Dataflow;
 
 namespace PlayingWithRabbitMQ.Queue.InMemory
 {
   public class BrokerFactory : IBrokerFactory
   {
-    private readonly ConcurrentDictionary<string, object> _queueDictionary;
-
-    public BrokerFactory()
-    {
-      _queueDictionary = new ConcurrentDictionary<string, object>();
-    }
+    private readonly ConcurrentDictionary<string, object> _queueDictionary
+      = new ConcurrentDictionary<string, object>();
 
     public IProducer<T> CreateProducer<T>() where T : class
-    {
-      Subject<T> queue = getQueueFor<T>(out var queueName);
-
-      return new Producer<T>(queue.AsObserver());
-    }
+      => new Producer<T>(getQueueFor<T>().AsObserver());
 
     public IConsumer<T> CreateConsumer<T>(Action connectionShutdown = null) where T : class, new()
-    {
-      Subject<T> queue = getQueueFor<T>(out var queueName);
+      => new Consumer<T>(getQueueFor<T>().AsObservable());
 
-      return new Consumer<T>(queue.AsObservable());
-    }
-
-    private Subject<T> getQueueFor<T>(out string queueName)
-    {
-      QueueMessageAttribute queueMessageAttr = typeof(T).GetCustomAttribute<QueueMessageAttribute>();
-
-      if (queueMessageAttr is null)
-        throw new ArgumentNullException($"QueueMessageAttribute is not present in the {typeof(T).Name}.");
-
-      queueMessageAttr.Validate();
-
-      queueName = queueMessageAttr.QueueName;
-
-      string key = $"{queueMessageAttr.ExchangeName}_{queueMessageAttr.RouteKey}";
-
-      // You can use BufferBlock, if that is suitable for you.
-      // Just for test purpose the Subject can be enough.
-      return _queueDictionary.GetOrAdd(key, new Subject<T>()) as Subject<T>;
-    }
+    private Subject<T> getQueueFor<T>()
+      => _queueDictionary.GetOrAdd(typeof(T).FullName, new Subject<T>()) as Subject<T>;
   }
 }
