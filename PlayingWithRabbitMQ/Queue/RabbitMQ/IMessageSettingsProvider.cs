59b7dd5480e516adeb8c7ca0c9f30f07fe710e10
Balázs Keresztúri <19balazs86@gmail.com>
2019-07-14 18:39:30 +0100

    Performance improvement to retrieve MessageSettingsAttribute.

--- PlayingWithRabbitMQ/Queue/RabbitMQ/IMessageSettingsProvider.cs
+++ PlayingWithRabbitMQ/Queue/RabbitMQ/IMessageSettingsProvider.cs
@@ -10,6 +10,13 @@ namespace PlayingWithRabbitMQ.Queue.RabbitMQ
   public class SimpleMessageSettingsProvider : IMessageSettingsProvider
   {
     public virtual MessageSettingsAttribute GetMessageSettingsFor<T>() where T : class
-      => typeof(T).GetCustomAttribute<MessageSettingsAttribute>();
+      => InnerClassProvider<T>.Value;
+
+    private static class InnerClassProvider<T>
+    {
+      // Benchmark for this trick: https://mariusgundersen.net/type-dictionary-trick
+      public static readonly MessageSettingsAttribute Value
+        = typeof(T).GetCustomAttribute<MessageSettingsAttribute>();
+    }
   }
 }
